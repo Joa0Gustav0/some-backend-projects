a857a0520d067a7ac8ac69a90307c8550e2b6f43
Joao <dev.joaogustavo.dev@gmail.com>
2024-02-21 15:03:39 -0300

    registering system | created base interface and added style to the project

---
+++ registering-system-project/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./styles/global.css">
+  <link rel="stylesheet" href="./styles/formulary.css">
+  <title>Registro Já | Criação de Registro</title>
+</head>
+<body>
+  <header class="header">
+    <div class="header__container">
+      <h1 class="header__container__logo-text">RegistroJá.com</h1>
+      <nav class="header__container__nav">
+        <a class="header__container__nav__navlinks" href="./registers-overview.php">
+          Ver Registros
+        </a>
+        <a class="header__container__nav__navlinks" href="./index.php">
+          Criar Registro
+        </a>
+      </nav>
+    </div>
+  </header>
+  <main class="main">
+    <div class="main__greeting-texts">
+      <h1 class="main__greeting-texts__title-text">👋 Registre-se Aqui!</h1>
+      <p class="main__greeting-texts__form-status-text">😥 Mensagem de Teste</p>
+    </div>
+    <form action="index.php" method="POST">
+      <label for="nome">Nome</label>
+      <input type="text" name="nome" id="nome" autocomplete="off" placeholder="Insira seu nome..." maxlength="20">
+      <p>⚠ O campo deve ser preenchido!</p>
+      <label for="idade">Idade</label>
+      <input type="number" name="idade" id="idade" placeholder="Insira sua idade..." min="0" max="150"/>
+      <p>⚠ O campo deve ser preenchido!</p>
+      <label for="email">Email</label>
+      <input type="text" name="email" id="email" autocomplete="off" placeholder="Insira seu email..." maxlength="50"/>
+      <p>⚠ O campo deve ser preenchido!</p>
+      <label for="hobbie">Hobbie</label>
+      <input type="text" name="hobbie" id="hobbie" autocomplete="off" placeholder="Insira um hobbie" maxlength="20"/>
+      <p>⚠ O campo deve ser preenchido!</p>
+      <input type="submit" name="submit" id="submit" value="Registrar Já!"/>
+    </form>
+  </main>
+</body>
+</html>
---
+++ registering-system-project/registers-overview.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./styles/global.css">
+  <title>Registro Já | Registros</title>
+</head>
+<body>
+  <header class="header">
+    <div class="header__container">
+      <h1 class="header__container__logo-text">RegistroJá.com</h1>
+      <nav class="header__container__nav">
+        <a class="header__container__nav__navlinks" href="./registers-overview.php">
+          Ver Registros
+        </a>
+        <a class="header__container__nav__navlinks" href="./index.php">
+          Criar Registro
+        </a>
+      </nav>
+    </div>
+  </header>
+  <main class="main">
+    <h1 class="main__title-text">👀 Veja Alguns Registros!</h1>
+  </main>
+</body>
+</html>
